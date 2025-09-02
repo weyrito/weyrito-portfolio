fafae276ee876adc3a83b316db615235a0dcf223
THOMAS FOUQUET <thomas.fouquet@ecole-hexagone.com>
2025-09-02 19:10:56 +0200

    big change

--- src/components/sections/index.ts
+++
@@ -1,7 +0,0 @@
-export { default as AboutSection } from './AboutSection';
-export { default as SkillsSection } from './SkillsSection';
-export { default as ProjectsSection } from './ProjectsSection';
-export { default as ExperienceSection } from './ExperienceSection';
-export { default as EducationSection } from './EducationSection';
-export { default as LanguagesSection } from './LanguagesSection';
-export { default as InterestsSection } from './InterestsSection';
